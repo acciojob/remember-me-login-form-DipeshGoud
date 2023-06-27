2ab05c8dbe16996fa9cfa2740fa9042631320ba3
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-06-27 13:49:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-//your JS code here. If required.
-
 check();
 
 function check() {
-	username = localStorage.getItem("username");
-	password = localStorage.getItem("password");
+  const username = localStorage.getItem("username");
+  const password = localStorage.getItem("password");
 
-	if (username && password) {
-		let but = document.createElement("button");
-		but.id = "existing"
-		but.textContent = "Login as existing user";
-		but.addEventListener("click", () => {
-			alert("Logged in as " + savedUsername);
-		})
+  if (username && password) {
+    const existingUserButton = document.createElement("button");
+    existingUserButton.id = "existing";
+    existingUserButton.textContent = "Login as existing user";
+    existingUserButton.addEventListener("click", () => {
+      alert("Logged in as " + username);
+    });
 
-		document.body.appendChild(but);
-	}
+    document.body.appendChild(existingUserButton);
+  }
 }
 
-document.getElementsByTag(form)[0].addEventListener("submit", (event) =>{
-	event.preventDefault();
-	let c = document.getElementById("checkbox");
-	let n = document.getElementById("username");
-	let p = document.getElementById("password");
-	
-		if(this.checked){
-		localStorage.setItem("username", n.value);
-		localStorage.setItem("password", p.value);
-		}else{
-		localStorage.removeItem("username", n.value);
-		localStorage.removeItem("password", p.value);
-		}
+document.getElementsByTagName("form")[0].addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  const rememberCheckbox = document.getElementById("checkbox");
+  const usernameInput = document.getElementById("username");
+  const passwordInput = document.getElementById("password");
+  
+  if (rememberCheckbox.checked) {
+    localStorage.setItem("username", usernameInput.value);
+    localStorage.setItem("password", passwordInput.value);
+  } else {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+  }
 
-	alert("Logged in as " + username);
-	})
+  alert("Logged in as " + usernameInput.value);
+});
